1867ca4f347d8db072270aec6145115c6857ae2a
Aswin Karthik Vijayakumar <aswinkarthikv@gmail.com>
2026-02-17 10:37:55 +0530

    Update lab5_first_follow_advanced.c

--- LAB 5/lab5_first_follow_advanced.c
+++ LAB 5/lab5_first_follow_advanced.c
@@ -3,61 +3,77 @@
 #include <ctype.h>
 
 int n;
-char prod[20][20];
-char first[20][20];
-char follow[20][20];
-char nt[20];
+char prod[10][20];
+char nonT[10];
+char FIRST[10][20];
+char FOLLOW[10][20];
 
-/* ---------- FIRST ---------- */
-void addToSet(char set[], char c) {
+/* add symbol to set */
+void add(char set[], char c) {
     if (!strchr(set, c)) {
-        int len = strlen(set);
-        set[len] = c;
-        set[len + 1] = '\0';
+        int l = strlen(set);
+        set[l] = c;
+        set[l + 1] = '\0';
     }
 }
 
-void findFirst(char c, int idx) {
+/* find FIRST */
+void findFIRST(char c, int idx) {
     if (!isupper(c)) {
-        addToSet(first[idx], c);
+        add(FIRST[idx], c);
         return;
     }
 
     for (int i = 0; i < n; i++) {
         if (prod[i][0] == c) {
-            int j = 2;
-            while (prod[i][j] != '\0') {
-                if (!isupper(prod[i][j])) {
-                    addToSet(first[idx], prod[i][j]);
-                    break;
-                } else {
-                    findFirst(prod[i][j], idx);
-                    if (!strchr(first[prod[i][j] - 'A'], '#'))
+            char rhs = prod[i][2];
+
+            if (!isupper(rhs)) {
+                add(FIRST[idx], rhs);
+            } else {
+                int k;
+                for (k = 0; k < n; k++)
+                    if (nonT[k] == rhs)
                         break;
-                }
-                j++;
+                findFIRST(rhs, k);
+
+                for (int x = 0; x < strlen(FIRST[k]); x++)
+                    add(FIRST[idx], FIRST[k][x]);
             }
         }
     }
 }
 
-/* ---------- FOLLOW ---------- */
-void findFollow(char c, int idx) {
-    if (c == nt[0])
-        addToSet(follow[idx], '$');
+/* find FOLLOW */
+void findFOLLOW(char c, int idx) {
+    if (c == nonT[0])
+        add(FOLLOW[idx], '$');
 
     for (int i = 0; i < n; i++) {
         for (int j = 2; j < strlen(prod[i]); j++) {
             if (prod[i][j] == c) {
+
                 if (prod[i][j + 1] != '\0') {
-                    if (!isupper(prod[i][j + 1])) {
-                        addToSet(follow[idx], prod[i][j + 1]);
+                    char next = prod[i][j + 1];
+
+                    if (!isupper(next)) {
+                        add(FOLLOW[idx], next);
                     } else {
-                        strcat(follow[idx], first[prod[i][j + 1] - 'A']);
+                        int k;
+                        for (k = 0; k < n; k++)
+                            if (nonT[k] == next)
+                                break;
+
+                        for (int x = 0; x < strlen(FIRST[k]); x++)
+                            add(FOLLOW[idx], FIRST[k][x]);
                     }
                 }
-                if (prod[i][j + 1] == '\0' && prod[i][0] != c) {
-                    findFollow(prod[i][0], idx);
+                else if (prod[i][0] != c) {
+                    int k;
+                    for (k = 0; k < n; k++)
+                        if (nonT[k] == prod[i][0])
+                            break;
+                    findFOLLOW(prod[i][0], idx);
                 }
             }
         }
@@ -68,24 +84,24 @@ int main() {
     printf("Enter number of productions: ");
     scanf("%d", &n);
 
-    printf("Enter productions (use # for epsilon):\n");
+    printf("Enter productions (E=TR):\n");
     for (int i = 0; i < n; i++) {
         scanf("%s", prod[i]);
-        nt[i] = prod[i][0];
-        first[i][0] = '\0';
-        follow[i][0] = '\0';
+        nonT[i] = prod[i][0];
+        FIRST[i][0] = '\0';
+        FOLLOW[i][0] = '\0';
     }
 
     printf("\nFIRST SETS\n");
     for (int i = 0; i < n; i++) {
-        findFirst(nt[i], i);
-        printf("FIRST(%c) = { %s }\n", nt[i], first[i]);
+        findFIRST(nonT[i], i);
+        printf("FIRST(%c) = { %s }\n", nonT[i], FIRST[i]);
     }
 
     printf("\nFOLLOW SETS\n");
     for (int i = 0; i < n; i++) {
-        findFollow(nt[i], i);
-        printf("FOLLOW(%c) = { %s }\n", nt[i], follow[i]);
+        findFOLLOW(nonT[i], i);
+        printf("FOLLOW(%c) = { %s }\n", nonT[i], FOLLOW[i]);
     }
 
     return 0;
